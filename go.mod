dca42ec96f95a934d287b96b1fdb3c21da4b6947
Boris Cazic <boris.c99@gmail.com>
2022-02-09 15:31:44 +0100

    commit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cazicbor/BORIS_LEVEL_UP
 go 1.17
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/echo/v4 v4.6.3 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
@@ -13,4 +14,5 @@ require (
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 )
--- go/src/main.go
+++ go/src/main.go
@@ -1,75 +1,142 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-	"strconv"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+    "encoding/json"
+    "bytes"
+    "io/ioutil"
 )
 
-type (
-	user struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	}
-)
+//task struct
 
-var (
-	users = map[int]*user{}
-	seq   = 1
-)
-
-//----------
-// Handlers
-//----------
-
-func createUser(c echo.Context) error {
-	u := &user{
-		ID: seq,
-	}
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	users[u.ID] = u
-	seq++
-	return c.JSON(http.StatusCreated, u)
+type Task struct {
+    ID string `json:"id"`
+    Description string `json:"description"`
+    Deadline string `json:"deadline"`
+    Status string `json:"status"`
 }
 
-func getUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	return c.JSON(http.StatusOK, users[id])
+type taskHandlers struct {
+    store map[string]Task
 }
 
-func updateUser(c echo.Context) error {
-	u := new(user)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	id, _ := strconv.Atoi(c.Param("id"))
-	users[id].Name = u.Name
-	return c.JSON(http.StatusOK, users[id])
+//slice used to store the tasks (à voir car pas de persistance des données à l'étape 1)
+var tasks []Task
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Benvenue sur la page d'accueil !")
+    fmt.Println("Endpoint Hit: homePage")
 }
 
-func deleteUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	delete(users, id)
-	return c.NoContent(http.StatusNoContent)
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/tasks", getAllTasks)
+    http.HandleFunc("/post", createTask)
+    log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
+
+//handlers
+
+func getAllTasks(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: getAllTasks")
+    json.NewEncoder(w).Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+    postBody, _ := json.Marshal(map[string]string{
+        "ID":  "5",
+        "Description": "jsp",
+        "Deadline" : "01/01/01",
+        "Status" : "To do",
+     })
+     responseBody := bytes.NewBuffer(postBody)
+  //Leverage Go's HTTP Post function to make request
+     resp, err := http.Post("localhost:8080/post", "application/json", responseBody)
+  //Handle Error
+     if err != nil {
+        log.Fatalf("An Error Occured %v", err)
+     }
+     defer resp.Body.Close()
+  //Read the response body
+     body, err := ioutil.ReadAll(resp.Body)
+     if err != nil {
+        log.Fatalln(err)
+     }
+     sb := string(body)
+     log.Printf(sb)
+}
+
+// func createTask(w http.ResponseWriter, r *http.Request) {
+//     jsonData := []Task{Task{"5", "jsp", "12/02/2022", "ToDo"}}
+//     jsonValue, _ := json.Marshal(jsonData)
+//     request, _ := http.NewRequest("POST", "localhost:8080/post", bytes.NewBuffer(jsonValue))
+//     request.Header.Set("Content-Type", "application/json")
+//     client := &http.Client{}
+//     response, err := client.Do(request)
+
+//     if err!= nil {
+//         fmt.Println("Request has failed with %s", err)
+//     } else {
+//         data, _ := ioutil.ReadAll(response.Body)
+//         fmt.Println(string(data))
+//     }
+// }
+
+
+// func createTask(w http.ResponseWriter, r *http.Request) error {
+// 	var t task
+//     if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+//         internalServerError(w, r)
+//         return
+//     }
+//     h.store.Lock()
+//     h.store.m[t.ID] = t
+//     h.store.Unlock()
+//     jsonBytes, err := json.Marshal(t)
+//     if err != nil {
+//         internalServerError(w, r)
+//         return
+//     }
+//     w.WriteHeader(http.StatusOK)
+//     w.Write(jsonBytes)
+// }
+
+
+// func updateTask(w http.ResponseWriter, r *http.Request) error {
+// 	u := new(user)
+// 	if err := c.Bind(u); err != nil {
+// 		return err
+// 	}
+// 	id, _ := strconv.Atoi(c.Param("id"))
+// 	users[id].Name = u.Name
+// 	return c.JSON(http.StatusOK, users[id])
+// }
+
+// func deleteTask(w http.ResponseWriter, r *http.Request) error {
+// 	id, _ := strconv.Atoi(c.Param("id"))
+// 	delete(users, id)
+// 	return c.NoContent(http.StatusNoContent)
+// }
+
+func internalServerError(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("internal server error"))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte("not found"))
+}
+
 
 func main() {
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Routes
-	e.POST("/users", createUser)
-	e.GET("/users/:id", getUser)
-	e.PUT("/users/:id", updateUser)
-	e.DELETE("/users/:id", deleteUser)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+	tasks = []Task{
+		{ID: "1", Description: "Construire une API REST en utilisant uniquement la librairie standard, sans persistance des données", Deadline: "09/02/2022", Status: "Ongoing"},
+		{ID: "2", Description: "Faire évoluer l'API : intégrer le routeur go-chi", Deadline: "09/02/2022", Status: "To do"},
+ 		{ID: "3", Description: "Créer tests unitaires", Deadline: "10/02/2022", Status: "To do"},
+ 		{ID: "4", Description: "Intégrer une persistance des données", Deadline: "10/02/2022", Status: "To do"},
+	}
+    handleRequests()
 }
