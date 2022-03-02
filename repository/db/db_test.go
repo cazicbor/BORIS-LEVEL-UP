810c6c2d006113b0850a5764e348b437e5a2f13c
coddity <augustin.de-laubier@coddity.com>
2022-03-02 11:28:30 +0100

    continuing testify suite

--- repository/db/db_test.go
+++ repository/db/db_test.go
@@ -1,102 +1,63 @@
 package db
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type DbTestSuite struct {
+type MongoHandlerSuite struct {
 	suite.Suite
+	db *mongo.Database
 }
 
-func TestDbTestSuite(t *testing.T) {
-	suite.Run(t, new(DbTestSuite))
+func (s *MongoHandlerSuite) SetupSuite() { //here we run everything that is "global", common to each test
+	//ici mettre fonction qui init l'environnement : NewMongoRepo, quid de address ?
+	//...
 }
 
-func TestNewHandler(t *testing.T) {
-
+//SetupTest runs before every unit test, in order to "clean" the DB, not to write over existing data
+func (s *MongoHandlerSuite) SetupTest() {
+	//ici intitaliser le MongoHandler de test?
+	err := s.db.Drop(context.TODO())
+	if err != nil {
+		s.T().Fatal(err)
+	}
 }
 
-func TestGetTaskByID(t *testing.T) {
-
-	/* mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
-	defer mt.Close()
-
-	mt.Run("success", func(mt *mtest.T) {
-		//taskCollection := mt.Coll
-		expectedData := &model.Task{
-			ID:          1,
-			Description: "test1",
-			Deadline:    "test1",
-			Status:      "test1",
-		}
-
-		mt.AddMockResponses(mtest.CreateCursorResponse(1, "foo.bar", mtest.FirstBatch, bson.D{
-			{"id", expectedData.ID},
-			{"description", expectedData.Description},
-			{"deadline", expectedData.Deadline},
-			{"status", expectedData.Status},
-		}))
-
-		taskResponse, err := GetTaskByID(expectedData.ID)
-
-		assert.Nil(t, err)
-		assert.Equal(t, &expectedData, taskResponse)
-	}) */
+//TearDownSuite disconnects from db
+func (s *MongoHandlerSuite) TearDownSuite() {
+	err := s.db.Client().Disconnect(context.TODO())
+	if err != nil {
+		s.T().Fatal(err)
+	}
 }
 
-func TestGetAllTasksByID(t *testing.T) {
+func TestMongoHandlerSuite(t *testing.T) {
+	s := new(MongoHandlerSuite)
+	suite.Run(t, s)
+}
 
-	/*var sliceTestTasks []*model.Task
-
-	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
-	defer mt.Close()
-
-	mt.Run("success", func(mt *mtest.T) {
-		taskCollection = mt.Coll
-		firstExpectedData := &model.Task{
-			ID:          1,
-			Description: "test1",
-			Deadline:    "test1",
-			Status:      "test1",
-		}
-		secondExpectedData := &model.Task{
-			ID:          2,
-			Description: "test2",
-			Deadline:    "test2",
-			Status:      "test2",
-		}
-
-		first := mtest.CreateCursorResponse(1, "foo.bar", mtest.FirstBatch, bson.D{
-			{"id", firstExpectedData.ID},
-			{"description", firstExpectedData.Description},
-			{"deadline", firstExpectedData.Deadline},
-			{"status", firstExpectedData.Status},
-		})
-
-		second := mtest.CreateCursorResponse(1, "foo.bar", mtest.FirstBatch, bson.D{
-			{"id", secondExpectedData.ID},
-			{"description", secondExpectedData.Description},
-			{"deadline", secondExpectedData.Deadline},
-			{"status", secondExpectedData.Status},
-		})
-
-		killCursors := mtest.CreateCursorResponse(0, "foo.bar", mtest.NextBatch)
-		mt.AddMockResponses(first, second, killCursors)
-
-		tasks, err :=
-	*/
+func (s *MongoHandlerSuite) TestNewMongoRepo() {
 
 }
 
-func TestAddTaskToDB(t *testing.T) {
+func (s *MongoHandlerSuite) TestGetTaskByID() {
+}
+
+func (s *MongoHandlerSuite) TestGetAllTasksByID() {
 
 }
 
-func TestUpdateTaskByID(t *testing.T) {
+func (s *MongoHandlerSuite) TestAddTaskToDB() {
 
 }
 
-func TestDeleteTaskByID(t *testing.T) {
+func (s *MongoHandlerSuite) TestUpdateTaskByID() {
+
+}
+
+func (s *MongoHandlerSuite) TestDeleteTaskByID() {
 }
